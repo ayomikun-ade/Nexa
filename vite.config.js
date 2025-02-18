ed9ad73bdd52c19dcde87b0fe3b84dd915d24c0d
Ayomikun <ayoadeosun10@gmail.com>
2025-02-18 23:49:58 +0100

    chore: added origin trials

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  html: {
+    env: {
+      VITE_TRANSLATOR_ORIGIN_TRIAL_TOKEN: import.meta.env
+        .VITE_TRANSLATOR_ORIGIN_TRIAL_TOKEN,
+      VITE_DETECTOR_ORIGIN_TRIAL_TOKEN: import.meta.env
+        .VITE_DETECTOR_ORIGIN_TRIAL_TOKEN,
+      VITE_SUMMARIZER_ORIGIN_TRIAL_TOKEN: import.meta.env
+        .VITE_SUMMARIZER_ORIGIN_TRIAL_TOKEN,
+    },
+  },
+});
