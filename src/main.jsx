f43b9875edf5640c15e486d013ffb61fd7d02b56
Ayomikun <ayoadeosun10@gmail.com>
2025-02-19 21:30:53 +0100

    chore: added origin trial tokens to meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const originTranslatorMeta = document.createElement("meta");
+originTranslatorMeta.httpEquiv = "origin-trial";
+originTranslatorMeta.content =
+  import.meta.env.VITE_TRANSLATOR_ORIGIN_TRIAL_TOKEN;
+document.head.append(originTranslatorMeta);
+
+const originDetectorMeta = document.createElement("meta");
+originDetectorMeta.httpEquiv = "origin-trial";
+originDetectorMeta.content = import.meta.env.VITE_DETECTOR_ORIGIN_TRIAL_TOKEN;
+document.head.append(originDetectorMeta);
+
+const originSummarizerMeta = document.createElement("meta");
+originSummarizerMeta.httpEquiv = "origin-trial";
+originSummarizerMeta.content =
+  import.meta.env.VITE_SUMMARIZER_ORIGIN_TRIAL_TOKEN;
+document.head.append(originSummarizerMeta);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // html: {
-  //   env: {
-  //     VITE_TRANSLATOR_ORIGIN_TRIAL_TOKEN: import.meta.env
-  //       .VITE_TRANSLATOR_ORIGIN_TRIAL_TOKEN,
-  //     VITE_DETECTOR_ORIGIN_TRIAL_TOKEN: import.meta.env
-  //       .VITE_DETECTOR_ORIGIN_TRIAL_TOKEN,
-  //     VITE_SUMMARIZER_ORIGIN_TRIAL_TOKEN: import.meta.env
-  //       .VITE_SUMMARIZER_ORIGIN_TRIAL_TOKEN,
-  //   },
-  // },
 });
